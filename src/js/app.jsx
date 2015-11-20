f8e2100b33f45664a4643821f41fd73990b7e8b7
Pranav Gandhi <pgandhi@hubspot.com>
2015-11-19 21:21:43 -0500

    workds

---
+++ src/js/app.jsx
@@ -0,0 +1,7 @@
+'use es6';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DegreePls from './components/DegreePls/DegreePls.jsx'
+
+ReactDOM.render(<DegreePls />, document.getElementById("content"));
---
+++ src/js/components/DegreePls/DegreePls.jsx
@@ -0,0 +1,10 @@
+'use es6';
+
+import React from 'react';
+
+export default React.createClass({
+    render: function() {
+        return <h1>Hallo!</h1>
+    }
+});
+
